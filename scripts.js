6dd1705ccd4ccd1e85473a22f05e714f78763cbe
WagnerGeroldi <wagnergeroldi@gmail.com>
2021-07-13 13:45:46 -0300

    teste novo

--- scripts.js
+++ scripts.js
@@ -13,6 +13,7 @@ today.innerHTML = dateNow;
 
 function createLi() {
     const li = document.createElement('li');
+    li.innerHTML = `<button type="checkbox"></button>`
     return li;
   }
 
@@ -26,13 +27,6 @@ function buttonDelete(li) {
     buttonDelete.innerText = 'Apagar';
     buttonDelete.setAttribute('class', 'button-list');
     li.appendChild(buttonDelete);
-}
-
-function buttonCheck(buttonDelete) {
-    const buttonTaskComplete = document.createElement('button');
-    buttonTaskComplete.innerText = 'Concluído';
-    buttonTaskComplete.setAttribute('class', 'button-check');
-    buttonDelete.appendChild(buttonTaskComplete);
 }
 
 function saveTask() {
@@ -52,7 +46,7 @@ function saveTask() {
 
 function addSaveTasks() {
     const tasks = localStorage.getItem('tarefas') ;
-    const listOfTasks = JSON.parse(tasks) || null;
+    const listOfTasks = JSON.parse(tasks);
 
     for (let task in listOfTasks) {
         createTask(task);
@@ -65,11 +59,10 @@ addSaveTasks();
 function createTask(textInput) {
     result.classList.add('content-right')
     const li = createLi();
-    li.innerText = textInput;
+    li.innerText = String(textInput);
     ulTask.appendChild(li);
     clearInput();
     buttonDelete(li);
-    buttonCheck(li);
     saveTask();
 }
 
@@ -84,13 +77,6 @@ document.addEventListener('click', function (e) {
     if (el.classList.contains('button-list')) {
         el.parentElement.remove();
         saveTask();
-    }
-})
-
-document.addEventListener('click', function (e) {
-    const el = e.target;
-    if (el.classList.contains('button-check')) {
-        $('li').first().addClass('concluido')
     }
 })
 
