9d79173b51e004471103f0adda8268a4acdc7b7d
WagnerGeroldi <wagnergeroldi@gmail.com>
2021-07-15 14:57:59 -0300

    add dark-mode

--- scripts.js
+++ scripts.js
@@ -95,4 +95,14 @@ document.addEventListener('click', function (e) {
     }
 })
 
+//dark mode
+
+const nightMode = document.querySelector('#night-mode')
+
+// ao clicar mudaremos as cores
+nightMode.addEventListener('click', () => {
+  // adiciona a classe `night-mode` ao html
+  document.documentElement.classList.toggle('night-mode')
+})
+
 
